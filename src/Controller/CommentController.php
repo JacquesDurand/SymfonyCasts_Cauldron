20763510106b1c0825cb6f729ec6f1975b361278
JacquesDurand <hugo.nicolas.hn@gmail.com>
2020-09-01 15:53:44 +0200

    Little play with the logger service

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -4,6 +4,7 @@
 namespace App\Controller;
 
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,16 +17,18 @@ class CommentController extends AbstractController
      * @param $direction
      * @return JsonResponse
      */
-    public function commentVote($id,$direction) {
+    public function commentVote($id,$direction, LoggerInterface $logger) {
         //to do : use the id to query the db
 
         //to do : replace next lines by real up vote logic and save in db
 
         if ($direction === 'up') {
             $currentVoteCount = rand(7,100);
+            $logger->info("Up !");
         }
         else {
             $currentVoteCount = rand(0,5);
+            $logger->info("Down !");
         }
 
         return $this->json(['votes' => $currentVoteCount]);
